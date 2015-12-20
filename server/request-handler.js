f60099fdce9f1b9b8da10141b2b35b8b2d7725ae
Preethi <iam.preethi.k@gmail.com>
2015-12-20 13:55:10 -0800

    clean up commentary

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,4 +1,4 @@
-// import node module requirements 
+// import node module requirements
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
@@ -8,11 +8,17 @@ var messages = [];
 var filePath = path.join(__dirname + '/bin/messages.json');
 
 // read file asynchronously
+var messageFileStream = fs.createReadStream(filePath, 'utf8');
+
+messageFileStream.on('readable', function(chunk) {
+  console.log(chunk);
+});
+
 fs.readFile(filePath, 'utf8', function(err, data) {
-if (err) { // handle error immediately 
-  return;
-}
-messages = JSON.parse(data);
+  if (err) { // handle error immediately
+    return;
+  }
+  messages = JSON.parse(data);
 });
 
 // save all our messages to our file system
@@ -25,6 +31,16 @@ var saveMessages = function() {
   });
 };
 
+// Since we are not using Express, we make sure to always call
+// response.end() - Node may not send
+// anything back to the client until we do. The string we pass to
+// response.end() will be the body of the response
+//
+// Calling .end "flushes" the response's internal buffer, forcing
+// node to actually send all the data over to the client.
+
+
+// GET all messages from local storage and send in response
 var GET = function(statusCode, headers, response) {
   statusCode = 200;
   headers['Content-Type'] = "application/json";
@@ -33,6 +49,7 @@ var GET = function(statusCode, headers, response) {
   response.end(responseData);
 };
 
+// post a new message to local storage
 var POST = function(statusCode, headers, request, response) {
   statusCode = 200;
   var lastId = messages[0].objectId || 0;
@@ -61,7 +78,6 @@ var POST = function(statusCode, headers, request, response) {
 function serveStaticPage(request, response) {
     //get the resource name specified in request.url
     var resource = url.parse(request.url).pathname;
-    console.log(resource);
     //if request is root, serve index page
     if (resource === '/') {
         resource = '/client/2015-10-chatterbox-client/client/index.html';
@@ -82,10 +98,10 @@ function serveStaticPage(request, response) {
               '.css': 'text/css'
             };
             var resourceExtension = path.extname(resource);
-            response.writeHead(200, {'Content-Type': extension[resourceExtension] });                      
+            response.writeHead(200, {'Content-Type': extension[resourceExtension] });
             response.end(content);
         }
-    });   
+    });
 };
 
 // define request handler
@@ -98,18 +114,22 @@ var requestHandler = function(request, response) {
     response.writeHead(statusCode, headers);
     response.end();
 
-  } else if (request.method === 'GET' && request.url === '/classes/messages') { 
-  // determine GET vs. POST response and handle appropriately 
+  } else if (request.method === 'GET' && request.url === '/classes/messages') {
+  // determine GET vs. POST response and handle appropriately
     GET(statusCode, headers, response);
   } else if (request.method === 'POST') {
     POST(statusCode, headers, request, response);
   } else {
-    console.log(request.method);
-    console.log(request.url);
     serveStaticPage(request, response);
   }
 
 };
+
+// Our chat client is running from a url like file://your/chat/client/index.html,
+// which is considered a different domain.
+// So these headers will allow Cross-Origin Resource Sharing (CORS).
+// This code allows this server to talk to websites that
+// are on different domains, for instance, our chat client.
 
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
@@ -121,26 +141,8 @@ var defaultCorsHeaders = {
 
 module.exports = requestHandler;
 
-  // Tell the client we are sending them plain text.
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
 
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
+// Another way to get around this CORS restriction is to serve you chat
 // client from this domain by setting up static file serving.
- // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
+// .writeHead() writes to the request line and headers of the response,
+// which includes the status and all headers.
